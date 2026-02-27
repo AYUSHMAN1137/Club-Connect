8a4982dd4ff1674622ab6b7a0044e50c778ee538
Ayush <ayushman1137@gmail.com>
2026-02-27 17:38:36 +0530

    deploy

--- android/app/src/main/java/com/clubconnect/app/MainActivity.java
+++ android/app/src/main/java/com/clubconnect/app/MainActivity.java
@@ -1,5 +1,15 @@
 package com.clubconnect.app;
 
+import android.os.Bundle;
+
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+import androidx.core.view.WindowCompat;
+
+public class MainActivity extends BridgeActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        WindowCompat.setDecorFitsSystemWindows(getWindow(), true);
+    }
+}
